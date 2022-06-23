9dd3c05676d1cd88b01da18edd795b8c31498241
Fabricio Tonossu <fabriciotonossu94@gmail.com>
2022-06-22 21:44:01 -0400

    Melhorias JS

--- Projeto/js/main.js
+++ Projeto/js/main.js
@@ -1,14 +1,73 @@
 let visor = document.querySelector('.visor');
 const numeros = document.querySelectorAll('.numero');
-
-let igual = document.querySelector('.igual')
-console.log(igual)
+let visorInicial = visor.value;
 
 
-for(let i = 0; i < numeros.length; i++){
-    numeros[i].addEventListener('click', function(){
-    
-        visor.setAttribute('value', numeros[i].value)
-        console.log(numeros[i].value)
+const operadores = document.querySelectorAll('.operador');
+let valorVisor1 = visorInicial;
+let valorVisor2 = visorInicial;
+let operacao = '';
+
+atibuicaoDeValores();
+
+function atibuicaoDeValores(){
+
+    if (operacao == 'soma'){
+        for(let i = 0; i < numeros.length; i++){
+            numeros[i].addEventListener('click', function(){
+                adicionaValor(numeros[i].value);
+                visor.setAttribute('value', valorVisor1);
+                console.log(operacao)
+            })
+        }
+        function adicionaValor(numero){
+            if (valorVisor1 != visorInicial) {
+                valorVisor1 = valorVisor1 + numero;
+            }else {
+                    valorVisor1 = numero;
+            }
+        }
+    } else {
+        console.log('mudou')
+    }
+}
+
+for(let i = 0; i < operadores.length; i++){
+    operadores[i].addEventListener('click', function(){
+        operacao = operadores[i].value;
     })
 }
+
+
+
+let igual = document.querySelector('.igual');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const reset = document.querySelector('.reset')
+
+reset.addEventListener('click', function(){
+    valorVisor1 = visorInicial;
+    visor.setAttribute('value', valorVisor1);
+    operacao = '';
+})
+
