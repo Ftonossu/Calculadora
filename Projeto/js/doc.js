20cb5e6ee57d92ef420107a31b5378021e21f60f
Fabricio Tonossu <fabriciotonossu94@gmail.com>
2022-06-26 14:46:23 -0400

    Finalizado

--- Projeto/js/doc.js
+++ Projeto/js/doc.js
@@ -1,26 +1,22 @@
-const teclasNumeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-console.log(teclasNumeros[1])
+// const teclasNumeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+// console.log(teclasNumeros[1])
 
-document.onkeydown = teclado;
+// document.onkeydown = teclado;
 
-function teclado(e){
-    for(let i = 0; i < teclasNumeros.length; i++){
-        if (e.keyCode == 48 + parseInt(teclasNumeros[i])){
-           console.log(teclasNumeros[i])
-    }
-}
-}
+// function teclado(e){
+//     for(let i = 0; i < teclasNumeros.length; i++){
+//         if (e.keyCode == 48 + parseInt(teclasNumeros[i])){
+//             adicionaValor1(teclasNumeros[i]);
+//             visor.setAttribute('value', valorVisor1);
+//     }
+// }
+// }
 
-function mostraValor1(numeros){
-    adicionaValor1(numeros.value);
-    visor.setAttribute('value', valorVisor1);
-    return valorVisor1;
-}
 
-function adicionaValor1(numero){
-    if (valorVisor1 != visorInicial) {
-        valorVisor1 = valorVisor1 + numero;
-    }else {
-        valorVisor1 = numero;
-    }
-}
+// function adicionaValor1(numero){
+//     if (valorVisor1 != visorInicial) {
+//         valorVisor1 = valorVisor1 + numero;
+//     }else {
+//         valorVisor1 = numero;
+//     }
+// }
